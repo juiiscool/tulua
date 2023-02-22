5594abe24b013319bb1b2aa9716f553ad1385e23
juiiscool <joanna.halili111@gmail.com>
2023-02-22 12:09:55 +0100

    hedhja e pare

---
+++ javascript/js.js
@@ -0,0 +1,21 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "navbar-right") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar-right";
+    }
+  }
+
+
+function newpage(){
+  window.location.href='book class.html'
+}
+
+function NewpageAgain(){
+  window.location.href='schedule.html'
+}
+
+function newpageEvents(){
+  window.location.href='events.html'
+}
